2ec245ca2bf0d30f40d89ecb985ebc8a72bea020
Posh <mrposhboo@gmail.com>
2024-01-17 15:39:31 -0800

    new

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,39 +1,37 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 /**
- * linear_search - Search for a value in an array using linear search
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: The value to search for
+ * linear_search - Searches for a value in an array
+ *                 of integers using linear search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
  *
- * Return: The first index where value is located, or -1 if not found
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the first index where the value is located.
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	size_t i;
 
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	/* Check if the array is NULL */
+	if (array == NULL)
+		return (-1);
 
-        if (array[i] == value)
-            return i;
-    }
+	/* Iterate over each element of the array */
+	for (i = 0; i < size; i++)
+	{
+		/* Print the value being checked */
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-    return -1;
-}
+		/* If the value is found */
+		if (array[i] == value)
 
-/* Example usage */
-int main(void)
-{
-    int array[] = {10, 1, 42, 3, 4, 42, 6, 7, -1, 9};
-    size_t size = sizeof(array) / sizeof(array[0]);
+			/* Return the index of the value */
+			return (i);
+	}
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-
-    return 0;
+	/* Value not found, return -1 */
+	return (-1);
 }
 

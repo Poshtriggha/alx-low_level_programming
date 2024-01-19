f82be529a47f1ebf6c5dc56eb4d9d912133a1ca8
Posh <mrposhboo@gmail.com>
2024-01-19 04:28:42 -0800

    linear

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,49 +1,41 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
- * binary_search - Searches for a value in a sorted array
- *                 of integers using binary search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
+ * binary_search - Searches for a value in a sorted array using binary search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
  *
- * Return: If the value is not present or the array is NULL, -1.
- *         Otherwise, the index where the value is located.
+ * Return: The index where the value is located, or -1 if not present or if
+ * the array is NULL
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right;
-	
-	/* Check if the array is NULL */
-	if (array == NULL)
-		return (-1);
+	size_t left = 0, right = size - 1, mid;
 
-	/* Perform binary search */
-	for (left = 0, right = size - 1; right >= left;)
+	if (array == NULL)
+		return -1;
+
+	while (left <= right)
 	{
+		size_t i;
+
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
-		
-		/* Calculate the middle index */
-		i = left + (right - left) / 2;
-		
-		/* If the middle element is the value */
-		if (array[i] == value)
-			/* Return the index */
-			return (i);
-		
-		/* If the middle element is greater than the value */
-		if (array[i] > value)
-			/* Update the right boundary */
-			right = i - 1;
+
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return mid;
+		else if (array[mid] < value)
+			left = mid + 1;
 		else
-			/* Otherwise, update the left boundary */
-			left = i + 1;
+			right = mid - 1;
 	}
-	/* Value not found, return -1 */
-	return (-1);
+
+	return -1; /* Value not found */
 }
 
